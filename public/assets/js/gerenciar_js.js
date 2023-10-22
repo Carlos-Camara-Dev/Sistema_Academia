0d3e8f0065439be30a83c89516c931c5036e83e0
Carlos-Camara-Dev <carloscamara.dev@gmail.com>
2023-10-22 13:49:22 -0300

    gerenciar.html, gerenciar_style.css e gerenciar_js.js criados: Estrutura do gerenciar.html criada e seu estilo em 50% e falta interliga-lo ao gerenciar_js

---
+++ public/assets/js/gerenciar_js.js
@@ -0,0 +1,10 @@
+function abrir_gerenciar_aluno() {
+    var gerenciar_alunos = document.getElementById("gerenciar_alunos");
+    if (gerenciar_alunos.style.display == "flex") {
+        gerenciar_alunos.style.display = "none";
+    }
+} function abrir_gerenciar_treino() {
+
+} function abrir_cadastrar_aluno() {
+
+}
