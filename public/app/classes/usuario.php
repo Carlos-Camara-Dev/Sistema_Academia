5748ba7c0f7aad2aca14bbadbc879a30e72d43ef
Carlos-Camara-Dev <carloscamara.dev@gmail.com>
2023-10-28 15:05:47 -0300

    Gerenciar>html atualizado: aba 'cadastrar aluno' mudado para 'cadastrar usuario', e 'cadastrar adm e aluno' criados e estilizados

---
+++ public/app/classes/usuario.php
@@ -0,0 +1,6 @@
+<?php
+class usuario
+{
+    private $nome;
+    private $senha;
+}
--- public/assets/js/gerenciar_js.js
+++ public/assets/js/gerenciar_js.js
@@ -1,6 +1,8 @@
 
-let gerenciar_alunos = document.getElementById("gerenciar_alunos"), gerenciar_treinos = document.getElementById("gerenciar_treinos"), cadastrar_aluno = document.getElementById("cadastrar_aluno"),
-    cadastrar_treino = document.getElementById("cadastrar_treino");
+let gerenciar_alunos = document.getElementById("gerenciar_alunos"), gerenciar_treinos = document.getElementById("gerenciar_treinos"), cadastrar_usuario = document.getElementById("cadastrar_usuario"),
+    cadastrar_treino = document.getElementById("cadastrar_treino"),
+    cadastrar_aluno = document.getElementById("gerenciar_cadastrar_aluno"),
+    cadastrar_adm = document.getElementById("gerenciar_cadastrar_adm");
 
 function abrir_gerenciar_aluno() {
     if (gerenciar_alunos.style.display != "flex") {
@@ -8,8 +10,8 @@ function abrir_gerenciar_aluno() {
         if (gerenciar_treinos.style.display != "none") {
             gerenciar_treinos.style.display = "none";
         }
-        if (cadastrar_aluno.style.display != "none") {
-            cadastrar_aluno.style.display = "none";
+        if (cadastrar_usuario.style.display != "none") {
+            cadastrar_usuario.style.display = "none";
         }
         if (cadastrar_treino.style.display != "none") {
             cadastrar_treino.style.display = "none";
@@ -21,16 +23,16 @@ function abrir_gerenciar_aluno() {
         if (gerenciar_alunos.style.display != "none") {
             gerenciar_alunos.style.display = "none";
         }
-        if (cadastrar_aluno.style.display != "none") {
-            cadastrar_aluno.style.display = "none";
+        if (cadastrar_usuario.style.display != "none") {
+            cadastrar_usuario.style.display = "none";
         }
         if (cadastrar_treino.style.display != "none") {
             cadastrar_treino.style.display = "none";
         }
     }
-} function abrir_cadastrar_aluno() {
-    if (cadastrar_aluno.style.display != "flex") {
-        cadastrar_aluno.style.display = "flex";
+} function abrir_cadastrar_usuario() {
+    if (cadastrar_usuario.style.display != "flex") {
+        cadastrar_usuario.style.display = "flex";
         if (gerenciar_alunos.style.display != "none") {
             gerenciar_alunos.style.display = "none";
         }
@@ -50,8 +52,23 @@ function abrir_gerenciar_aluno() {
         if (gerenciar_treinos.style.display != "none") {
             gerenciar_treinos.style.display = "none";
         }
+        if (cadastrar_usuario.style.display != "none") {
+            cadastrar_usuario.style.display = "none";
+        }
+    }
+}
+function abrir_cadastrar_adm() {
+    if (cadastrar_adm.style.display != "flex") {
+        cadastrar_adm.style.display = "flex";
         if (cadastrar_aluno.style.display != "none") {
             cadastrar_aluno.style.display = "none";
+        }
+    }
+} function abrir_cadastrar_aluno() {
+    if (cadastrar_aluno.style.display != "flex") {
+        cadastrar_aluno.style.display = "flex";
+        if (cadastrar_adm.style.display != "none") {
+            cadastrar_adm.style.display = "none";
         }
     }
 }
