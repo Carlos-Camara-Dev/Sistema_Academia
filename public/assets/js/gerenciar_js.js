374dda24faf01084debfcfb2cac264b38c7223fe
Carlos-Camara-Dev <carloscamara.dev@gmail.com>
2023-10-28 12:13:52 -0300

    Gerenciar.html atualizado: form -> cadastrar treino criado e estilizado

--- public/assets/js/gerenciar_js.js
+++ public/assets/js/gerenciar_js.js
@@ -1,5 +1,6 @@
 
-let gerenciar_alunos = document.getElementById("gerenciar_alunos"), gerenciar_treinos = document.getElementById("gerenciar_treinos"), cadastrar_aluno = document.getElementById("cadastrar_aluno");
+let gerenciar_alunos = document.getElementById("gerenciar_alunos"), gerenciar_treinos = document.getElementById("gerenciar_treinos"), cadastrar_aluno = document.getElementById("cadastrar_aluno"),
+    cadastrar_treino = document.getElementById("cadastrar_treino");
 
 function abrir_gerenciar_aluno() {
     if (gerenciar_alunos.style.display != "flex") {
@@ -9,6 +10,9 @@ function abrir_gerenciar_aluno() {
         }
         if (cadastrar_aluno.style.display != "none") {
             cadastrar_aluno.style.display = "none";
+        }
+        if (cadastrar_treino.style.display != "none") {
+            cadastrar_treino.style.display = "none";
         }
     }
 } function abrir_gerenciar_treino() {
@@ -20,6 +24,9 @@ function abrir_gerenciar_aluno() {
         if (cadastrar_aluno.style.display != "none") {
             cadastrar_aluno.style.display = "none";
         }
+        if (cadastrar_treino.style.display != "none") {
+            cadastrar_treino.style.display = "none";
+        }
     }
 } function abrir_cadastrar_aluno() {
     if (cadastrar_aluno.style.display != "flex") {
@@ -29,6 +36,22 @@ function abrir_gerenciar_aluno() {
         }
         if (gerenciar_treinos.style.display != "none") {
             gerenciar_treinos.style.display = "none";
+        }
+        if (cadastrar_treino.style.display != "none") {
+            cadastrar_treino.style.display = "none";
+        }
+    }
+} function abrir_cadastrar_treino() {
+    if (cadastrar_treino.style.display != "flex") {
+        cadastrar_treino.style.display = "flex";
+        if (gerenciar_alunos.style.display != "none") {
+            gerenciar_alunos.style.display = "none";
+        }
+        if (gerenciar_treinos.style.display != "none") {
+            gerenciar_treinos.style.display = "none";
+        }
+        if (cadastrar_aluno.style.display != "none") {
+            cadastrar_aluno.style.display = "none";
         }
     }
 }
