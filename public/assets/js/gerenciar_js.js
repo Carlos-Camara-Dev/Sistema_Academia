c7c2fc2e08b7902774b2fd1ebb008cc22de404bc
Carlos-Camara-Dev <carloscamara.dev@gmail.com>
2023-11-02 18:26:48 -0300

    Gerenciar.html atualizadfo: gerenciar e criar adm's criados, estilizados e conectados ao JavaScript

--- public/assets/js/gerenciar_js.js
+++ public/assets/js/gerenciar_js.js
@@ -1,12 +1,44 @@
 
-let gerenciar_alunos = document.getElementById("gerenciar_alunos"), gerenciar_alunos_lista = document.getElementById("gerenciar_alunos_lista"), gerenciar_treinos_lista = document.getElementById("gerenciar_treinos_lista"), gerenciar_treinos = document.getElementById("gerenciar_treinos"),
-    cadastrar_adm = document.getElementById("gerenciar_cadastrar_adm"),
+let cadastrar_adm = document.getElementById("cadastrar_adm"), gerenciar_adms = document.getElementById("gerenciar_adms"), gerenciar_adms_lista = document.getElementById("gerenciar_adms_lista"),
     cadastrar_aluno = document.getElementById("cadastrar_aluno"),
-    cadastrar_treino = document.getElementById("cadastrar_treino");
+    gerenciar_alunos = document.getElementById("gerenciar_alunos"), gerenciar_alunos_lista = document.getElementById("gerenciar_alunos_lista"),
+    cadastrar_treino = document.getElementById("cadastrar_treino"), gerenciar_treinos_lista = document.getElementById("gerenciar_treinos_lista"), gerenciar_treinos = document.getElementById("gerenciar_treinos");
+
+// Gerenciar Adm's:
+function abrir_gerenciar_adms() {
+    if (gerenciar_adms.style.display != "flex") {
+        gerenciar_adms.style.display = "flex";
+        if (gerenciar_alunos.style.display != "none") {
+            gerenciar_alunos.style.display = "none";
+        }
+        if (gerenciar_treinos.style.display != "none") {
+            gerenciar_treinos.style.display = "none";
+        }
+    }
+}
+function abrir_gerenciar_adms_lista() {
+    if (gerenciar_adms_lista.style.display != "flex") {
+        gerenciar_adms_lista.style.display = "flex";
+        if (cadastrar_adm.style.display != "none") {
+            cadastrar_adm.style.display = "none";
+        }
+    }
+
+} function abrir_cadastrar_adm() {
+    if (cadastrar_adm.style.display != "flex") {
+        cadastrar_adm.style.display = "flex";
+        if (gerenciar_adms_lista.style.display != "none") {
+            gerenciar_adms_lista.style.display = "none";
+        }
+    }
+}
 // Gerenciar Alunos:
 function abrir_gerenciar_alunos() {
     if (gerenciar_alunos.style.display != "flex") {
         gerenciar_alunos.style.display = "flex";
+        if (gerenciar_adms.style.display != "none") {
+            gerenciar_adms.style.display = "none";
+        }
         if (gerenciar_treinos.style.display != "none") {
             gerenciar_treinos.style.display = "none";
         }
@@ -33,6 +65,9 @@ function abrir_gerenciar_alunos_lista() {
 function abrir_gerenciar_treino() {
     if (gerenciar_treinos.style.display != "flex") {
         gerenciar_treinos.style.display = "flex";
+        if (gerenciar_adms.style.display != "none") {
+            gerenciar_adms.style.display = "none";
+        }
         if (gerenciar_alunos.style.display != "none") {
             gerenciar_alunos.style.display = "none";
         }
