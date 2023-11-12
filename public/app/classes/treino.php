1af73ca90c967b97fc986f470865e7e48fdba72f
Carlos-Camara-Dev <carloscamara.dev@gmail.com>
2023-11-11 22:25:06 -0300

    classe treino,verificar.php e gernciar.html atualoizados: classe treino, gerenciar.html e verificar.php conectados com BD

--- public/app/classes/treino.php
+++ public/app/classes/treino.php
@@ -1,6 +1,62 @@
-
 <?php
-class treino
+require_once("../models/banco_conexao.php");
+
+class Treino
 {
-    private Academia $academia;
+    private $nome;
+    private $id;
+    private $descricao;
+    private $tipo;
+    public function __construct($treino_nome, $treino_id, $treino_descricao, $treino_tipo, $conexao)
+    {
+
+        $this->set_nome($treino_nome);
+        $this->set_id($treino_id);
+        $this->set_descricao($treino_descricao);
+        $this->set_tipo($treino_tipo);
+    }
+    public function get_nome()
+    {
+        return $this->nome;
+    }
+    public function set_nome($nome)
+    {
+        $this->nome = $nome;
+    }
+    public function get_id()
+    {
+        return $this->id;
+    }
+    public function set_id($id)
+    {
+        $this->id = $id;
+    }
+    public function get_descricao()
+    {
+        return $this->descricao;
+    }
+    public function set_descricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+    public function get_tipo()
+    {
+        return $this->tipo;
+    }
+    public function set_tipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+
+    public function treino_cadastrar($treino_nome, $treino_id, $treino_descricao, $treino_tipo, $conexao)
+    {
+        $cadastrar_personal = $conexao->query("INSERT INTO personal(treino_nome, treino_id, treino_tipo, treino_descricao) VALUES('$treino_nome', '$treino_id','$treino_tipo','$treino_descricao')");
+        echo '<script  type="text/javascript">
+                alert("A $treino_nome foi criada. Seja bem-vindo!");
+                </script>';
+        header('Location: ../views/gerenciar.html');
+    }
+    public function buscar_dados()
+    {
+    }
 }
--- public/app/function/verificar.php
+++ public/app/function/verificar.php
@@ -3,32 +3,38 @@ require_once("../models/banco_conexao.php");
 require_once("../classes/academia.php");
 require_once("../classes/aluno.php");
 require_once("../classes/personal.php");
+require_once("../classes/treino.php");
 $operacao = $_GET['operacao'];
 
+
+// Dados da Academia:
 if (isset($_POST['academia_nome'])) {
-    // Dados da Academia:
     $academia_nome = $_POST['academia_nome'];
     $academia_cnpj = $_POST['academia_cnpj'];
     $academia_senha = $_POST['academia_senha'];
     $academia_email = $_POST['academia_email'];
     $academia_status;
 }
+// Dados dos Alunos:
 if (isset($_POST['aluno_nome'])) {
-
-    // Dados dos Alunos:
     $aluno_nome = $_POST['aluno_nome'];
     $aluno_id = $_POST['aluno_id'];
     // $aluno_senha = $_POST['aluno_senha'];
     $aluno_email = $_POST['aluno_email'];
-    $aluno_status = false;
 }
+// Dados dos Personal:
 if (isset($_POST['personal_nome'])) {
-    // Dados dos Personal:
     $personal_nome = $_POST['personal_nome'];
     $personal_id = $_POST['personal_id'];
     $personal_senha = $_POST['personal_senha'];
     $personal_email = $_POST['personal_email'];
-    $personal_status = false;
+}
+// Dados dos Treino:
+if (isset($_POST['treino_nome'])) {
+    $treino_nome = $_POST['treino_nome'];
+    $treino_id = $_POST['treino_id'];
+    $treino_descricao = $_POST['treino_descricao'];
+    $treino_tipo = $_POST['treino_tipo'];
 }
 // Verificar Academia:
 
@@ -73,6 +79,22 @@ switch ($operacao) {
         } else {
             $personal = new Personal($personal_nome, $personal_id, $personal_senha, $personal_email, $conexao);
             $personal->personal_cadastrar($personal_nome, $personal_id, $personal_senha, $personal_email, $conexa);
+        }
+
+
+        break;
+    case 'treino':
+        $verificar_treino_id = $conexao->query("SELECT * FROM treino WHERE treino_nome= '$treino_nome' AND treino_id='$treino_id'");
+
+        if ($verificar_treino_id->rowCount() > 0) {
+
+            echo '<script  type="text/javascript">' .
+                "alert('O $treino_nome já foi cadastrado!');" .
+                'window.history.back();
+                    </script>';
+        } else {
+            $treino = new Treino($treino_nome, $treino_id, $treino_descricao, $treino_tipo, $conexao);
+            $treino->treino_cadastrar($treino_nome, $treino_id, $treino_descricao, $treino_tipo, $conexa);
         }
 
 
